d2ccc3509a9977831b3274eb5c5194e5329f604d
Marco Lanconelli <m.lanconelli@nier.it>
2025-03-12 16:28:09 +0100

    scaffolding skeleton v3

---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<header class="bg-red-500 p-4">(header)</header>
+	<!-- Grid Column -->
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr_auto]">
+		<!-- Sidebar (Left) -->
+		<aside class="bg-yellow-500 p-4">(sidebar)</aside>
+		<!-- Main -->
+		<main class="space-y-4 bg-green-500 p-4">
+			<p class="h-[512px] bg-purple-500 p-4">
+            </p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
+			<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+		</main>
+		<!-- Sidebar (Right) -->
+		<aside class="bg-yellow-500 p-4">(sidebar)</aside>
+	</div>
+	<!-- Footer -->
+	<footer class="bg-blue-500 p-4">(footer)</footer>
+</div>
